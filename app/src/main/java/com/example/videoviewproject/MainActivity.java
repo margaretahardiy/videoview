93905344a35c6b217029bdaeda7cca4da8f0443e
margaretahardiy <margaretahardiyanti@Margaretas-MacBook-Air.local>
2023-04-09 12:36:42 +0700

    Create video player from url & internal storage

---
+++ app/src/main/java/com/example/videoviewproject/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.videoviewproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.URLUtil;
+import android.widget.MediaController;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mBufferingTextView;
+
+//    private static final String VIDEO_SAMPLE = "draw_video";
+    private VideoView mVideoView;
+    private int mCurrentPosition = 0;
+    private static final String PLAYBACK_TIME = "play_time";
+
+    private static final String VIDEO_SAMPLE = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mVideoView = findViewById(R.id.videoview);
+        mBufferingTextView = findViewById(R.id.buffering_textview);
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+        mVideoView.setMediaController(controller);
+
+    }
+
+    private Uri getMedia(String mediaName) {
+        if (URLUtil.isValidUrl(mediaName)) {
+           return Uri.parse(mediaName);
+        } else { // media name is a raw resource embedded in the app
+            return Uri.parse("android.resource://" + getPackageName() +
+                    "/raw/" + mediaName);
+        }
+    }
+
+    private void initializePlayer() {
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mBufferingTextView.setVisibility(VideoView.VISIBLE);
+        mVideoView.setVideoURI(videoUri);
+
+        if (mCurrentPosition > 0) {
+            mVideoView.seekTo(mCurrentPosition);
+        } else {
+            // Skipping to 1 shows the first frame of the video.
+            mVideoView.seekTo(1);
+        }
+
+        mVideoView.setOnPreparedListener(
+                new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mBufferingTextView.setVisibility(VideoView.INVISIBLE);
+
+                        if (mCurrentPosition > 0) {
+                            mVideoView.seekTo(mCurrentPosition);
+                        } else {
+                            mVideoView.seekTo(1);
+                        }
+
+                        mVideoView.start();
+                    }
+                });
+
+        mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                Toast.makeText(MainActivity.this, "Playback completed",
+                        Toast.LENGTH_SHORT).show();
+                mVideoView.seekTo(1);
+            }
+        });
+
+//        mVideoView.start();
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+    }
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        initializePlayer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        releasePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
+
+}
